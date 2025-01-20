1ee5c65f7ccfb6f665e67ff31d93297b5d441d50
Victor Javier <vjaviertaype@outlook.com>
2025-01-19 21:16:14 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+
+import ReactPlayer from 'react-player'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [link, setLink] = useState("https://www.youtube.com/watch?v=I3AsBLb9OM4");
+  const [playing, setPlaying] = useState(false);
+
+  const handlePlay = () => {
+    setPlaying(!playing);
+  }
+  const handleLink = (e) => {
+    e.preventDefault();
+
+    if (e.target[0].value === "") return;
+
+    setLink(e.target[0].value);
+    setPlaying(false);
+  }
+
+  return (
+    <>
+      <div className='header-app'>
+        <h1>
+          <a href="https://github.com/cookpete/react-player">React Player</a>
+        </h1>
+        <p>Aprendiendo a utilizar React Player: es un componente para reproducir videos y audios, es muy fácil de usar.</p>
+        <q>Es mantenido por <portal><a href="https://www.mux.com/">Mux</a></portal></q>
+        <form className='form-link' onSubmit={handleLink}>
+          <input type="url" name="link-video" id="link-video" placeholder='Link del video' />
+          <button type="submit">Cargar</button>
+        </form>
+      </div>
+      <div className='player-app'>
+        <ReactPlayer playing={playing} controls url={link}></ReactPlayer >
+        <button onClick={handlePlay}>Play/Pause</button>
+      </div>
+    </>
+  )
+}
+
+export default App
